1943a3fc39415f083d158587fe1b388f7f2d8877
Wendal Chen <wendal1985@gmail.com>
2020-03-05 19:21:04 +0800

    fix: main.c没有调用luat_main -_-

--- applications/main.c
+++ applications/main.c
@@ -10,24 +10,23 @@
 
 #include <rtthread.h>
 #include <rtdevice.h>
-#include <board.h>
+#include "luat_base.h"
 
-/* defined the LED0 pin: PC0 */
-#define LED0_PIN    GET_PIN(C, 0)
+static void _main(void* param) {
+    rt_thread_mdelay(100); // 故意延后100ms
+    luat_main(NULL, NULL, NULL);
+    while (1)
+        rt_thread_delay(10000000);
+}
 
 int main(void)
 {
-    int count = 1;
-    /* set LED0 pin mode to output */
-    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
-
-    while (count++)
-    {
-        rt_pin_write(LED0_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED0_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
-    }
-
-    return RT_EOK;
+    rt_thread_t t = rt_thread_create("luat", _main, RT_NULL, 6*1024, 15, 20);
+    rt_thread_startup(t);
+    //luat_main(NULL, NULL, NULL);
+    //while (1) {
+    //    rt_thread_mdelay(2000000);
+    //}
+    return 0;
 }
+
